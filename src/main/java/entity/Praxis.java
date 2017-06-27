51e2717a4e98d4edd1492f9005d65a7e22ab65eb
angau <lucasv666@gmail.com>
2017-06-27 08:43:09 -0300

    Update Praxis.java

--- src/main/java/entity/Praxis.java
+++ src/main/java/entity/Praxis.java
@@ -8,37 +8,10 @@ import javax.persistence.GeneratedValue;
 /*esta clase representaria una practica, con un valor fijo*/
 
 @Entity
-public class Praxis extends Priceable{
-	
-	@Id
-	@GeneratedValue
-	@Column(name="ID")
-	private long id;
-	
-
-	@Column(nullable = false)
-	private String name;
-	
+public class Praxis extends Priceable{	
 
 	@Column(nullable = false)
 	private int code;
-	
-	public long getId(){
-		return this.id;
-	}
-	
-	public void setId(long id){
-		this.id = id;
-	}
-	
-	public String getName(){
-		return this.name;
-	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
 
 	public int getCode(){
 		return this.code;
