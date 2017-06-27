8cc28d5175e2d201ebf0f5f489d5e73cc7e478ac
angau <lucasv666@gmail.com>
2017-06-27 08:38:02 -0300

    Update Priceable.java

--- src/main/java/entity/Priceable.java
+++ src/main/java/entity/Priceable.java
@@ -1,7 +1,36 @@
 package entity;
 
+import javax.persistence.*;
+
 /**
- todavia no estoy muy seguro como funcionaria esto
+
+no es soportado por JPA una interfaz como entity. no sirve el enfoque. 
+Se lo combirtió a clase
  */
-public interface Priceable {
+@Entity
+public class Priceable {
+ 
+ @Id
+ @GeneratedValue
+ private long id;
+ 
+ @Column
+ private String name;
+ 
+ public long getId(){
+  return this.id;
+ }
+ 
+ public void setId(long id){
+  this.id=id;
+ }
+ 
+ public String getName(){
+  return this.name;
+ }
+ 
+ public void setName(String name){
+  this.name=name;
+ }
+ 
 }
